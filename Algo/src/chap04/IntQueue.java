c4abd14a6793501d4ae95ba49f209d20e37df7e0
MIN <MIN@DESKTOP-1O2IU6C>
2020-12-29 12:36:13 +0900

    chap4-2 큐 학습~~

--- Algo/src/chap04/IntQueue.java
+++ Algo/src/chap04/IntQueue.java
@@ -27,4 +27,78 @@ public class IntQueue {
 			max = 0;
 		}
 	}
+	
+	//큐에 데이터를 인큐
+	public int enque(int x) throws OverflowIntQueueException{
+		if (num >= max)
+			throw new OverflowIntQueueException();	//큐가 가득 참
+		que[rear++] = x;
+		num++;
+		if(rear == max)	//rear의 값이 max와 같아지면 실제 배열에는 없는 공간을 가리키게 됨
+			rear = 0;
+		return x;
+	}
+	
+	//큐에서 데이터를 디큐
+	public int deque() throws EmptyIntQueueException{
+		if (num <= 0)
+			throw new EmptyIntQueueException();	//큐가 비어 있음
+		int x = que[front++];
+		if(front == max)	//인덱스 초과 문제 해결
+			front = 0;	
+		return x;	
+	}
+	
+	//큐에서 데이터를 피크(프런트 데이터를 들여다봄)
+	public int peek() throws EmptyIntQueueException{
+		if(num <= 0)
+			throw new EmptyIntQueueException();	//큐가 비어 있음
+		return que[front++];
+	}
+	
+	//큐에서 x를 검색하여 인덱스(찾지 못하면-1)를 반환
+	public int indexOf(int x) {
+		for(int i=0; i<num; i++) {
+			int idx = (i + front) % max;
+			if(que[idx] == x)
+				return idx;		//검색성공
+		}
+		return -1;
+	}
+	
+	//큐를 비움
+	public void clear() {
+		num = front = rear = 0;
+	}
+	
+	//큐의 용량을 반환
+	public int capacity() {
+		return max;
+	}
+	
+	//큐에 쌓여 있는 데이터의 수를 반환
+	public int size() {
+		return num;
+	}
+	
+	//큐가 비어 있는가?
+	public boolean isEmpty() {
+		return num <= 0;
+	}
+	
+	//큐가 가득차 있는가?
+	public boolean isFull() {
+		return num >= max;
+	}
+	
+	//큐 안의 모든 데이터를 프런트->리어 순으로 출력
+	public void dump() {
+		if (num <= 0)
+			System.out.println("큐가 비어 있습니다.");
+		else {
+			for(int i=0; i<num; i++)
+				System.out.print(que[(i+front)%max] + " ");
+			System.out.println();
+		}
+	}
 }
